d8ca414a8f58ef818c75fd11dd5ae8d9bb485464
Paulo Ricardo de Souza Silva <30639809+pauloricardosouza@users.noreply.github.com>
2024-11-07 13:37:27 -0300

    Add files via upload

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 
     <!-- Rodapé do Sistema -->
     <div class="mt-5 bg-dark text-white text-center fixed-bottom">
-        <p>Genérico - Sistema Web de Vendas</p>
+        <p>Genérico - Sistema Web para Vendas</p>
     </div>
 
 </body>
